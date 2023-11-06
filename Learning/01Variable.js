6d702c01b6d580694195f78fba8337d2720ff488
Vineet <86636480+VineetPrajapati@users.noreply.github.com>
2023-11-06 11:14:48 +0000

    new commit

---
+++ Learning/01Variable.js
@@ -0,0 +1,10 @@
+const accountID = 12345;
+let accountEmail = "vineetprajapti@gmail.com";
+var accountPassword = "098776";
+accountCity = "Jaipur";
+
+let accountState;
+
+console.log(accountPassword);
+console.table([accountID,accountEmail, accountPassword, accountCity ,accountState]);
+/* try not to use "var" keyword due to it's scope issue. */
